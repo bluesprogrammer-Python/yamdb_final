03b6fabc1626f9c6106cba0772c5f634272385e7
Sergey Semenov <Raze.teatro2012@yandex.ru>
2022-10-11 23:36:00 +0300

    isort 2.0

--- api_yamdb/reviews/migrations/0001_initial.py
+++ api_yamdb/reviews/migrations/0001_initial.py
@@ -4,11 +4,10 @@ import django.contrib.auth.models
 import django.core.validators
 import django.db.models.deletion
 import django.utils.timezone
-from django.conf import settings
-from django.db import migrations, models
-
 import reviews.utils
 import reviews.validators
+from django.conf import settings
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
